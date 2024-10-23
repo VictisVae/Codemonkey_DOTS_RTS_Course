20276d735cfa51bc7190abe9749ff9136d19eef1
Yevhen <jamesche.yc@gmail.com>
2024-10-23 21:49:30 +0200

    Move job systen

--- Assets/Scripts/Systems/UnitMoveSystem.cs
+++ Assets/Scripts/Systems/UnitMoveSystem.cs
@@ -5,20 +5,49 @@ using Unity.Physics;
 using Unity.Transforms;
 
 namespace Systems {
-  partial struct UnitMoveSystem : ISystem {
+  internal partial struct UnitMoveSystem : ISystem {
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-      foreach ((RefRW<LocalTransform> localTransform, RefRO<UnitMover> unitMover, RefRW<PhysicsVelocity> velocity) in SystemAPI
-                 .Query<RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>()) {
-        float3 movementDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
+      UnitMoverJob unitMoverJob = new() {
+        deltaTime = SystemAPI.Time.DeltaTime
+      };
+
+      unitMoverJob.ScheduleParallel();
+
+      // foreach ((RefRW<LocalTransform> localTransform, RefRO<UnitMover> unitMover, RefRW<PhysicsVelocity> velocity) in SystemAPI
+      //            .Query<RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>()) {
+      //   float3 movementDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
+      //   movementDirection = math.normalize(movementDirection);
+      //
+      //   localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(movementDirection, math.up()),
+      //     SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
+      //
+      //   velocity.ValueRW.Linear = movementDirection * unitMover.ValueRO.moveSpeed;
+      //   velocity.ValueRW.Angular = float3.zero;
+      // }
+    }
+
+    [BurstCompile]
+    public partial struct UnitMoverJob : IJobEntity {
+      public float deltaTime;
+
+      private void Execute(ref LocalTransform localTransform, in UnitMover unitMover, ref PhysicsVelocity velocity) {
+        float3 movementDirection = unitMover.targetPosition - localTransform.Position;
+        float unitReachedDistance = 1.0f;
+
+        if (math.length(movementDirection) < unitReachedDistance) {
+          velocity.Linear = float3.zero;
+          velocity.Angular = float3.zero;
+          return;
+        }
+
         movementDirection = math.normalize(movementDirection);
 
-        localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(movementDirection, math.up()),
-          SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed);
+        localTransform.Rotation = math.slerp(localTransform.Rotation, quaternion.LookRotation(movementDirection, math.up()),
+          deltaTime * unitMover.rotationSpeed);
 
-        velocity.ValueRW.Linear = movementDirection * unitMover.ValueRO.moveSpeed;
-        velocity.ValueRW.Angular = float3.zero;
-        // localTransform.ValueRW.Position += movementDirection * movementSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+        velocity.Linear = movementDirection * unitMover.moveSpeed;
+        velocity.Angular = float3.zero;
       }
     }
   }
